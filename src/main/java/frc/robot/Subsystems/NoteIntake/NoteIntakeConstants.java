b7e14aad91a5e206a71fc1e2b074559e93d0b30b
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-04-24 18:32:56 +0300

    wrote the intake system need to add logger

---
+++ src/main/java/frc/robot/Subsystems/NoteIntake/NoteIntakeConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.Subsystems.NoteIntake;
+
+public final class NoteIntakeConstants {
+    public static final int INTAKE_MOTOR_ONE_ID = 0;
+    public static final int INTAKE_MOTOR_TWO_ID = 1;
+    public static final double INTAKE_MOTOR_SPEED = 0.15;
+}
