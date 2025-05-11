bc840feab345512741957bfcdb334ac21ffb244f
ilaybaruch <166049550+ilaybaruch@users.noreply.github.com>
2025-05-11 18:35:05 +0300

    added tolerance and commands

--- src/main/java/frc/robot/Subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterConstants.java
@@ -3,7 +3,8 @@ package frc.robot.Subsystems.Shooter;
 public final class ShooterConstants {
     public static final int RIGHT_MOTOR_ID = 5;
     public static final int LEFT_MOTOR_ID = 6;
-    public static final int Kp = 0;
-    public static final int Ki = 0;
-    public static final int Kd = 0;
+    public static final double Kp = 0;
+    public static final double Ki = 0;
+    public static final double Kd = 0;
+    public static final double TOLERANCE = 0;
 }
--- src/main/java/frc/robot/Subsystems/Shooter/ShooterIOReal.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterIOReal.java
@@ -26,6 +26,7 @@ public class ShooterIOReal implements ShooterIO {
             encoder = right_motor.getEncoder();
             config
             .idleMode(IdleMode.kCoast).follow(right_motor, true);
+            pidController.setTolerance(TOLERANCE);
             
             
         }
